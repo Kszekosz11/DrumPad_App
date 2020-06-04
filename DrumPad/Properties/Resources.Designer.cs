7ac5bf38b86ea02323106a4b67de324925a0ad41
Grzegorz Waluś <gwalus1404@gmail.com>
2020-06-04 18:43:56 +0200

    delete unneeded files

--- DrumPad/Properties/Resources.Designer.cs
+++ DrumPad/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SoundsTest.Properties {
+namespace DrumPad.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SoundsTest.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SoundsTest.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DrumPad.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
